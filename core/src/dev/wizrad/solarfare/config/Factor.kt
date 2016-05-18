6acc7cd1050f60dcd8d3a6d04c50b8b49e8db1dc
Ty Cobb <ty.cobb.m@gmail.com>
2016-05-18 08:50:16 -0500

    remove unsed factor code/config

--- core/src/dev/wizrad/solarfare/config/Factor.kt
+++ core/src/dev/wizrad/solarfare/config/Factor.kt
@@ -5,26 +5,9 @@ class Factor {
   lateinit var range: Range
   /** @property raw The scaling ratio fro the factor's raw value */
   var raw: Double = 1.0
-  /** @property available The scaling ratio for the factor's available cost */
-  var available: Double = 0.0
-  /** @property independent The scaling ratio for the factor's independent cost */
-  var independent: Double = 0.0
 
-  //
-  // Sampling
+  // MARK: Sampling
   fun sample(): Double {
     return range.sample() * raw
-  }
-
-  //
-  // Cost
-  /** Translates the given raw value into an available cost */
-  fun availableCost(value: Double): Int {
-    return (available * value / raw).toInt()
-  }
-
-  /** Translates the given raw value into an independent cost */
-  fun independentCost(value: Double): Int {
-    return (independent * value / raw).toInt()
   }
 }
